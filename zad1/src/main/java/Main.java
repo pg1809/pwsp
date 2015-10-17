b8cb0fe27d225db66c8a404f289dc68728dc2575
Piotr Grzelak <piotr.grzelak93@gmai.com>
2015-10-17 15:36:54 +0200

    Moja propozycja rozwiązania zadania. Do przedyskutowania.

--- zad1/src/main/java/Main.java
+++ zad1/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Semaphore;
@@ -8,20 +9,47 @@ import java.util.concurrent.Semaphore;
  */
 public class Main {
 
-    public static void main(String [] args){
-        Double[] array = {1.0, 6.0, 2.0, 5.0, 4.0, 3.0};
+    public static void main(String[] args) {
+        double[] array = {1.0, 6.0, 200.0, 99.0, 0.3, 5.0, 4.0, 3.0, 0.1};
 
-        int s = 3;
-        int t = 3;
+        int s = 4;
+        int t = 5;
 
-        Semaphore semaphore1 = new Semaphore(1, true);
-        Semaphore semaphore2 = new Semaphore(1, true);
+//        Semaphore semaphore1 = new Semaphore(1, true);
+//        Semaphore semaphore2 = new Semaphore(1, true);
+//
+//        Thread firstSetThread = new Thread(new FirstSet(array, 0, s, semaphore1, semaphore2));
+//        Thread secondSetThread = new Thread(new SecondSet(array, s, s+t, semaphore1, semaphore2));
+//
+//        firstSetThread.start();
+//        secondSetThread.start();
 
-        Thread firstSetThread = new Thread(new FirstSet(array, 0, s, semaphore1, semaphore2));
-        Thread secondSetThread = new Thread(new SecondSet(array, s, s+t, semaphore1, semaphore2));
+        SetManager firstSetManager = new SetManager(0, s, array);
+        firstSetManager.setElementsComparator((Double first, Double second) -> {
+            if (first < second) {
+                return 1;
+            } else if (first == second) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
 
-        firstSetThread.start();
-        secondSetThread.start();
+        SetManager secondSetManager = new SetManager(s, t, array);
+        secondSetManager.setElementsComparator((Double first, Double second) -> {
+            if (first > second) {
+                return 1;
+            } else if (first == second) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
 
+        firstSetManager.setAnother(secondSetManager);
+        secondSetManager.setAnother(firstSetManager);
+
+        firstSetManager.start();
+        secondSetManager.start();
     }
 }
---
+++ zad1/src/main/java/SetManager.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by Piotr Grzelak on 2015-10-17.
+ */
+public class SetManager extends Thread {
+
+    private static Semaphore changeSemaphore = new Semaphore(1, true);
+
+    private static ReusableBarrier barrier = new ReusableBarrier(2);
+
+    private double[] spaceOfElements;
+
+    private int setBeginningIdx;
+
+    private int setSize;
+
+    private SetManager another;
+
+    private int elemToChangeIdx;
+
+    private Semaphore readWriteSemaphore;
+
+    private Comparator<Double> elementsComparator;
+
+    public SetManager(int setBeginningIdx, int setSize, double[] spaceOfElements) {
+        this.setBeginningIdx = setBeginningIdx;
+        this.setSize = setSize;
+        this.spaceOfElements = spaceOfElements;
+
+        readWriteSemaphore = new Semaphore(1, true);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                readWriteSemaphore.acquire();
+                elemToChangeIdx = findElementToChangeIdx();
+
+                barrier.waitAtBarrier();
+                barrier.resumeFromBarrier();
+
+                if (elementsComparator.compare(spaceOfElements[elemToChangeIdx], spaceOfElements[another.elemToChangeIdx]) > 0) {
+                    return;
+                }
+                readWriteSemaphore.release();
+
+                another.readWriteSemaphore.acquire();
+                changeSemaphore.acquire();
+                if (elementsComparator.compare(spaceOfElements[elemToChangeIdx], spaceOfElements[another.elemToChangeIdx]) <= 0) {
+                    double tmp = spaceOfElements[elemToChangeIdx];
+                    spaceOfElements[elemToChangeIdx] = spaceOfElements[another.elemToChangeIdx];
+                    spaceOfElements[another.elemToChangeIdx] = tmp;
+                }
+                changeSemaphore.release();
+                another.readWriteSemaphore.release();
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("After execution: " + Arrays.toString(spaceOfElements));
+            readWriteSemaphore.release();
+        }
+    }
+
+    private int findElementToChangeIdx() {
+        int idx = setBeginningIdx;
+        for (int i = setBeginningIdx; i < setBeginningIdx + setSize; ++i) {
+            if (elementsComparator.compare(spaceOfElements[i], spaceOfElements[idx]) <= 0) {
+                idx = i;
+            }
+        }
+
+        return idx;
+    }
+
+    public void setAnother(SetManager another) {
+        this.another = another;
+    }
+
+    public void setElementsComparator(Comparator<Double> elementsComparator) {
+        this.elementsComparator = elementsComparator;
+    }
+}
